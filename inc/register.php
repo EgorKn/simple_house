de0adc9bf06458eb90c47afe4e3f64e09b75bf73
EgorKn <egor.knyshev@student.ukf.sk>
2023-05-25 09:18:32 +0200

    really final

--- inc/register.php
+++ inc/register.php
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html>
 <head>
     <title>Rergistration</title>
-    <link rel="stylesheet" type="text/css" href="css/admin.css">
+    <link rel="stylesheet" type="text/css" href="../css/admin.css">
 </head>
 <body>
     <h2>Registration</h2>
