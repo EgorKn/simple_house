c75dd1e7aea380413e4ee98e1d64fb9a9f21ce0c
EgorKn <egor.knyshev@student.ukf.sk>
2023-05-25 09:47:44 +0200

    .

--- inc/login.php
+++ inc/login.php
@@ -10,7 +10,6 @@ $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 try {
     $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
     exit;
--- inc/register.php
+++ inc/register.php
@@ -8,7 +8,6 @@ $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 try {
     $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Connection failed: ' . $e->getMessage();
     exit;
